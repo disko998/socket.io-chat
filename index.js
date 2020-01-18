5b4177237f6a7ef838877db26f9903b13c74b355
Stefan <disko998@gmail.com>
2020-01-18 21:34:06 +0100

    components style change

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const socket = io('http://localhost:3000')
+var socket = io('http://localhost:3000')
 
-const form = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
-const messageContainer = document.getElementById('message-container')
+var form = document.getElementById('send-container')
+var messageInput = document.getElementById('message-input')
+var messageContainer = document.getElementById('message-container')
 
-const username = prompt('What is you name?')
-appendMessage('You joined', 'info', new Date().toLocaleDateString())
+var username = prompt('What is your name?')
+username = username ? username : 'Guest'
+
+appendMessage('You join conversation', 'info', getTimeSpan())
 socket.emit('new-user', username)
 
 socket.on('user-connected', name => {
-    appendMessage(`${name} just joined`, 'info', new Date().toLocaleDateString())
+    appendMessage(`${name} join conversation`, 'info', getTimeSpan())
 })
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    const message = {data: messageInput.value, date: new Date().toLocaleDateString()}
-    appendMessage(message.data, 'myMessage', message.date)
-    socket.emit('chat-message-send', message)
+
+    if (validateForm()) {
+        var message = { data: messageInput.value, date: getTimeSpan() }
+
+        appendMessage(message.data, 'myMessage', message.date)
+        socket.emit('chat-message-send', message)
+    }
+
     messageInput.value = ''
 })
 
@@ -24,27 +31,41 @@ socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message.data}`, 'userMessage', data.message.date)
 })
 
- function appendMessage(message, type, date) {
-     const messageBoxElement = document.createElement('div')
-     const messageElement = document.createElement('p')
-     const dateContainer = document.createElement('span')
+function appendMessage(message, type, date) {
+    const messageBoxElement = document.createElement('div')
+    const messageElement = document.createElement('p')
+    const dateContainer = document.createElement('span')
 
-     messageElement.innerText = message
-     dateContainer.innerText = date
+    messageElement.innerText = message
+    dateContainer.innerText = date
 
-     messageBoxElement.classList.add('message')
-     messageBoxElement.classList.add(type)
-     dateContainer.classList.add('date')
+    messageBoxElement.classList.add('message')
+    messageBoxElement.classList.add(type)
+    dateContainer.classList.add('date')
 
-     messageElement.append(dateContainer)
-     messageBoxElement.append(messageElement)
-     messageContainer.append(messageBoxElement)
+    messageElement.append(dateContainer)
+    messageBoxElement.append(messageElement)
+    messageContainer.append(messageBoxElement)
 
-     scrollToBottom()
- }
+    scrollToBottom()
+}
 
- function scrollToBottom() {
-    console.log(messageContainer.scrollHeight)
-    messageContainer.scrollTo(0, messageContainer.scrollHeight);
- }
+function scrollToBottom() {
+    messageContainer.scrollTo(0, messageContainer.scrollHeight)
+}
 
+function getTimeSpan() {
+    var myDate = new Date()
+    var minutes = myDate.getMinutes()
+    var hours = myDate.getHours()
+
+    return `${hours}:${minutes}`
+}
+
+function validateForm() {
+    if (messageInput.value.trim()) {
+        return true
+    }
+
+    return false
+}
